18c563b46ead9efa2e690f40169e233391f85610
CamiloQspec12 <camiloandreszorrilla@gmail.com>
2026-08-10 20:47:59 +1000

    Income controller

--- src/controllers/expenses.js
+++ src/controllers/expenses.js
@@ -2,8 +2,11 @@ import prisma from "../prisma"
 
 const getExpenses = async (req, res) => {
     try {
-    const expenses = await prisma.expenses.findMany()
-    res.status(200).json({message: "Created succesfully..."})
+    const expenses = await prisma.expenses.findMany({
+        user: true,
+        category: true
+    })
+    res(expenses).json()
     }catch (e){
         res.status(500).json({message: "U get all the expenses...."})
     }
@@ -14,10 +17,12 @@ const createExpense = async (req, res) => {
 
         const expenses = await prisma.expense.create({data: {
             type: req.body.type,
-            value: req.body.value
+            value: req.body.value,
+            userExpense: req.body.userExpense,
+            categoryId: req.body.categoryId
         }})
+        res.status(200).json({message: "Created succesfully..."})
 
-        res(expenses).json()
 
     } catch(e){
         res.status(200).json({message: "Expense created"})
---
+++ src/controllers/income.js
@@ -0,0 +1,29 @@
+import prisma from "../prisma";
+
+const getIncome = async (req, res) => {
+    try {
+        const income = prisma.income.findMany({
+            include: {
+                user: true
+            }
+        })
+        res.json(income)
+    }
+    catch (e) {
+        res.status(500).json({message: 'Not getting anything...'})
+    }
+}
+
+
+const createIncome = async (req, res) => {
+    try {
+        const income = prisma.income.create({data: {
+            qt: req.body.qt,
+            frequency: req.body.frequency,
+            userIncome: req.body.userIncome
+        }})
+    }
+    catch(e) {
+        res.status(500).json({message: 'Error creating income'})
+    }
+}
