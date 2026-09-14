466cc7d02096f3452111b2a1d5998f6fa9b432ae
CamiloQspec12 <camiloandreszorrilla@gmail.com>
2026-09-14 22:04:54 +1000

    ssl adapter prisma

--- src/prisma.js
+++ src/prisma.js
@@ -1,8 +1,7 @@
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL, ssl: {rejectUnauthorized: false} })
 const prisma = new PrismaClient({ adapter })
-console.log('DATABASE_URL:', JSON.stringify(process.env.DATABASE_URL))
 
 export default prisma
