cff4e3a18313702cdb65eb16548bf63dd56e36bd
CamiloQspec12 <camiloandreszorrilla@gmail.com>
2026-09-15 21:55:38 +1000

    Cors config

--- src/index.js
+++ src/index.js
@@ -4,15 +4,20 @@ import cors from 'cors'
 import allRoutes from './routes/allRoutes.js'
 
 const app = express()
-const middleware = cors()
 const PORT = process.env.PORT || 3000
 
 
-app.use(middleware)
+const configCors = {
+    origin: [
+        'http://localhost:5173',
+        'https://expenses-control-frontend.vercel.app'
+    ]
+}
+
+
+app.use(cors(configCors))
 app.use(express.json())
-
 app.use('/api', allRoutes)
-
 app.get('/', (req, res) => {
     res.json({message: "The app is running"})
 })
